1c7e4f64e38317ddba1a997a7060aabcf6f4be64
vikingviolinist <misopetrik@gmail.com>
2022-07-22 22:29:56 +0200

    ✨ Add script

---
+++ app.js
@@ -0,0 +1,67 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+function PageTransitions() {
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener('click', (e) => {
+      const currentBtn = document.querySelector('.active-btn');
+      currentBtn.classList = currentBtn.className.replace('active-btn', '');
+      e.target.classList.add('active-btn');
+    });
+  }
+
+  allSections.addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+
+    if (id) {
+      sectBtns.forEach((btn) => {
+        btn.classList.remove('active');
+      });
+      //   e.target.classList.add('active');
+
+      sections.forEach((section) => {
+        section.classList.remove('active');
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add('active');
+    }
+  });
+
+  // Toggle theme
+  const themeBtn = document.querySelector('.theme-btn');
+
+  themeBtn.addEventListener('click', () => {
+    const element = document.body;
+    element.classList.toggle('light-mode');
+
+    const toggleButton = document.querySelector('.theme-icon');
+
+    if (toggleButton.classList.contains('fa-moon')) {
+      toggleButton.className = toggleButton.className.replace(
+        'fa-moon',
+        'fa-sun'
+      );
+    } else {
+      toggleButton.className = toggleButton.className.replace(
+        'fa-sun',
+        'fa-moon'
+      );
+    }
+
+    const leftClef = document.querySelector('.g-clef-left');
+    const rightClef = document.querySelector('.g-clef-right');
+
+    if (leftClef.src.includes('raster')) {
+      leftClef.src = leftClef.src.replace('-raster', '');
+      rightClef.src = rightClef.src.replace('clef', 'clef-raster');
+    } else {
+      rightClef.src = rightClef.src.replace('-raster', '');
+      leftClef.src = leftClef.src.replace('clef', 'clef-raster');
+    }
+  });
+}
+
+PageTransitions();
